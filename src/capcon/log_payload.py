8184553eba381c9f988bb0070e7ed35ee9fb8886
Peter Heller <52469803+Frozenbitz@users.noreply.github.com>
2026-04-10 14:14:29 +0200

    fix: updated the logging configuration for client and server, added a server option and passed in end of test logging.

--- src/capcon/log_payload.py
+++ src/capcon/log_payload.py
@@ -4,17 +4,18 @@ from capcon.util.payload import genPayload, GenericPayload
 logging_payloads: list[GenericPayload] = []
 logging_payloads.append(
     genPayload(
-        command="docker ps",
+        command="docker ps -a",  # usually we want to run ps -a to get failed containers in the output logs!
         description="get container health state",
         limits="1s",
         offset="200ms",
         payload_type="logs",
+        target=["client", "server"],
     )
 )
 
 logging_payloads.append(
     genPayload(
-        command="docker container logs plc-logic --tail 20",
+        command="docker container logs plc-logic --tail 10",
         description="check client plc-logic health",
         limits="1s",
         offset="200ms",
@@ -24,7 +25,7 @@ logging_payloads.append(
 
 logging_payloads.append(
     genPayload(
-        command="docker container logs plc-server --tail 20",
+        command="docker container logs plc-server --tail 10",
         description="check client plc-server health",
         limits="1s",
         offset="200ms",
@@ -34,10 +35,21 @@ logging_payloads.append(
 
 logging_payloads.append(
     genPayload(
-        command="docker container logs plc-historian --tail 20",
+        command="docker container logs plc-historian --tail 10",
         description="check client plc-historian health",
         limits="1s",
         offset="200ms",
         payload_type="logs",
+    )
+)
+
+logging_payloads.append(
+    genPayload(
+        command="journalctl -n 20 --no-pager",
+        description="Get the latest server logs, in case of failure",
+        limits="1s",
+        offset="{end_of_test_logs}s",
+        payload_type="logs",
+        target=["client", "server"],
     )
 )
